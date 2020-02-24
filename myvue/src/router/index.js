d0d9fff08d76dbb132688de81bbcc772f48d2ca1
jieyao000 <1541656728@qq.com>
2020-02-25 00:49:54 +0800

    .

---
+++ myvue/src/router/index.js
@@ -0,0 +1,84 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import  PageOne from '../views/PageOne'
+import  PageTwo from '../views/PageTwo'
+import  PageThree from '../views/PageThree'
+import  PageFour from '../views/PageFour'
+import Index from '../views/Index'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI);
+
+
+
+
+
+
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: '导航1',
+      redirect :"pageOne",
+    component: Index,
+      children : [
+
+          {
+              path :'/pageOne',
+              name : '页面1',
+              component: PageOne
+
+          },
+          {
+              path :'/pageTwo',
+              name : '页面2',
+              component : PageTwo
+
+
+
+          }
+      ]
+  },
+
+    {
+        path :"/navigation",
+        name:"导航2",
+        component :Index,
+        children:[
+            {
+                path :'/pageThree',
+                name : '页面3',
+                component : PageThree
+
+            },
+            {
+                path :'/pageFour',
+                name : '页面4',
+                component: PageFour
+
+            }
+
+
+        ]
+
+
+
+    }
+
+
+
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
